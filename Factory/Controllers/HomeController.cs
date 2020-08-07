f1e71c33b80f6fb90a5650983714d21ee4b1bc2e
Tyson Lackey <lackeyt90@gmail.com>
2020-08-07 11:26:33 -0700

    update HomeController

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -4,9 +4,18 @@ namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly FactoryContext _db;
+
+      public EngineersController(FactoryContext db)
+      {
+        _db = db;
+      }
+
       [HttpGet("/")]
       public ActionResult Index()
       {
+        ViewBag.Machines = _db.Machines.ToList();
+        ViewBag.Engineers = _db.Engineers.ToList();
         return View();
       }
     }
